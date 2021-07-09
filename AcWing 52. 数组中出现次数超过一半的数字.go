146b447d312d3dbdb16e84a8ac54411034a1281c
handongyexingren <1340551309@qq.com>
2021-07-09 09:49:50 +0800

    修改了出现次数超过一半的数

--- AcWing 52. 数组中出现次数超过一半的数字.go
+++ AcWing 52. 数组中出现次数超过一半的数字.go
@@ -1,16 +1,28 @@
 func moreThanHalfNum_Solution(nums []int) int {
     //要找的数字比其他数字总和还要多
-    times := 1
-    ans := nums[0]
-    for i := 1 ; i < len(nums) ; i++ {
-        if times == 0 {
-            ans = nums[i]
-            times = 1
-        }else if nums[i] == ans {
-            times++
-        }else {
-            times--
-        }
-    }
-    return ans
+    if len(nums) == 0 {
+		return -1
+	}
+	times := 0
+	result := -1
+	for _,num := range nums {
+		if times == 0 {
+			result = num
+			times = 1
+		}else if result == num {
+			times++
+		}else {
+			times--
+		}
+	}
+	times = 0
+	for _,num := range nums {
+		if num == result {
+			times++
+		}
+	}
+	if times*2 <= len(nums) {
+		return -1
+	}
+	return result
 }
